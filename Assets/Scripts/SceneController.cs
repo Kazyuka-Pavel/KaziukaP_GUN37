5a3ca2fd0333a5e73d87df1ffd565dbb980d71ff
KaziukaPP <pvlpka@yandex.ru>
2025-10-15 17:55:30 +0300

    1

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    public void OpenMainScene()
+    {
+        
+    }
+    
+    public void OpenGameScene()
+    {
+        
+    }
+}
---
+++ Assets/Settings/InjectConstants.cs
@@ -0,0 +1,5 @@
+
+public static class InjectConstants 
+{
+    public const string PlayerPrefab = "Player";
+}
