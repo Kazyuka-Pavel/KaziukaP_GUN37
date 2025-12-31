5bf31802e809fe59d21a2a5380e296733ad037bc
KaziukaPP <pvlpka@yandex.ru>
2025-12-31 20:40:15 +0300

    1

--- Assets/Editor/EditorCheatWindow.cs
+++ Assets/Editor/EditorCheatWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEditor;
@@ -41,11 +42,25 @@ public static class EditorCheatWindow
 
         //ConstructorInfo
         // СОздание ScriptableObject через рефлексию, используя метаданные
-        //type = typeof(UnitGameSettings);
+        type = typeof(UnitGameSettings);
         //var constr = type.GetConstructor(Array.Empty<Type>());
         //var settings = constr.Invoke(Array.Empty<object>());
 
         //FieldInfo
         var fieldInfo = type.GetField("focusMesh", BindingFlags.Instance | BindingFlags.NonPublic); // приватные свойства можно получить через настрйоку бинда
+        var renderer = fieldInfo.GetValue(cell) as MeshRenderer;
+        fieldInfo.SetValue(cell, default(MeshRenderer));
+        EditorUtility.SetDirty(cell); // оповещение об изменении (грязный)
+
+        type = typeof(EditorCheatWindow);
+        var methodInfo = type.GetMethod(nameof(EditorCheatWindows), BindingFlags.Instance | BindingFlags.NonPublic); // приватый метод
+        methodInfo.Invoke(typeof(EditorCheatWindow), new object[] { });  // для объектов  передается сам объект, для статических - тип
+
+        //Альтернативный вариант без рефлекии SerializedObject SerializedProperty
+        var serializedObject = new SerializedObject(cell);
+        serializedObject.Update(); //подтягивание всех изменений из cell
+        var property = serializedObject.FindProperty("focusMesh"); // не важно приватный или нет, если сериализуемый
+        property.objectReferenceValue = default(MeshRenderer);        
+        serializedObject.ApplyModifiedProperties(); // оповещение об изменении (грязный)
     }
 }
